b3b8f382a9f0b003cd2dee2d36f995dd182eb3a0
Aaron Nwabuoku <aaron.nwabuoku@herzberglabs.com>
2019-09-16 15:32:43 -0400

    Initial Commit

---
+++ howdi-sockets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  howdi-sockets
+//
+//  Created by Aaron Nwabuoku on 2019-09-16.
+//  Copyright © 2019 Herzberg Labs Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
